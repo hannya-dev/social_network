a24b84fc8f55e95fe253600ecba58a87102da73c
hannya-dev <levinbaka2001@gmail.com>
2022-07-05 19:55:34 +0500

    добавил NavLink и поверстал Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,27 @@ import './Dialogs.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className="dialogs">
+            <div className="dialogs-items">
+                <div className="dialog active">
+                    Sanek
+                </div>
+                <div className="dialog">
+                    Nikita
+                </div>
+                <div className="dialog">
+                    Anna
+                </div>
+                <div className="dialog">
+                    Vitek
+                </div>
+            </div>
+            <div className="messages">
+                <div className="message">Hi</div>
+                <div className="message">How are you?</div>
+                <div className="message">Yo</div>
+                <div className="message">Kachan</div>
+            </div>
         </div>
     )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import "./Navbar.css";
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className="nav">
             <div className="item">
-                <a href="/news">News</a>
-            </div>
-            <div className="item active">
-                <a href="/profile">Profile</a>
+                <NavLink to="/news">News</NavLink>
             </div>
             <div className="item">
-                <a href="/dialogs">Messages</a>
+                <NavLink to="/profile">Profile</NavLink>
             </div>
             <div className="item">
-                <a href="/music">Music</a>
+                <NavLink to="/dialogs">Messages</NavLink>
             </div>
             <div className="item">
-                <a href="/settings">Settings</a>
+                <NavLink to="/music">Music</NavLink>
+            </div>
+            <div className="item">
+                <NavLink to="/settings">Settings</NavLink>
             </div>
         </nav>
     )
