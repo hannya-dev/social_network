627c97f5bd6158e871d496de7d595f5c379d075a
hannya-dev <levinbaka2001@gmail.com>
2022-08-03 18:50:21 +0500

    рефакторинг кода

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import store from "./redux/state";
+import store from "./redux/store";
 import './index.css';
 import App from './App';
 
